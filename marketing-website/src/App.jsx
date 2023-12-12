d39560f31965988e6d194a5c37536b57a9988b32
Jafarscript <lihammedjafar@gmail.com>
2023-12-12 22:16:41 +0100

    First Project Initialized

---
+++ marketing-website/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import {SiYourtraveldottv} from 'react-icons/si'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className='logo'><SiYourtraveldottv className='user'></SiYourtraveldottv>Safar</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
