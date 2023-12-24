0777bba4604a9d80d38ca935f1f8d25ced624972
Jafarscript <lihammedjafar@gmail.com>
2023-12-24 12:02:43 +0100

    Product and About Us section done

--- marketing-website/src/sections/PopularProducts.jsx
+++ marketing-website/src/sections/PopularProducts.jsx
@@ -1,6 +1,23 @@
+import { products } from "../constants";
+import PopularProductCard from "../components/PopularProductCard";
+
 const PopularProducts = () => {
+
+  // console.log(products)
   return (
-    <div>PopularProducts</div>
+    <section id="product" className="max-container max-sm:mt-12 " >
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Products</h2>
+        <p className="lg-max-w-lg mt-2 font-montserrat text-slate-gray">Step into a World of Style: Discover Our Exclusive Footwear Collection! Elevate Your Every Step with Trendsetting Designs, Impeccable Quality, and Unmatched Comfort. Find Your Perfect Pair Today!</p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product}>
+
+          </PopularProductCard>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- marketing-website/src/sections/SuperQuality.jsx
+++ marketing-website/src/sections/SuperQuality.jsx
@@ -1,6 +1,26 @@
+import Button from '../components/Button';
+import {shoe8} from '../assets/images'
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+      <div className="flex flex-1 flex-col">
+        <h2 className="text-4xl font-palanquin font-bold capitalize lg:max-w-lg">
+          We Provide You <span className="text-coral-red">Super </span>
+          <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+        <p className="lg-max-w-lg mt-4 info-text">
+        At Oja, we prioritize your comfort and style. Our meticulously crafted shoes offer unmatched quality, innovation, and a touch of elegance. Step confidently into a world where each pair is designed to elevate your experience – because your every step deserves exceptional quality.
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'>Our commitment goes beyond shoes, aiming for confident, sophisticated strides in every pair, where your satisfaction is paramount.</p>
+        <div className='mt-11'>
+        <Button label='View details'/>
+        </div>
+      </div>
+
+      <div className='flex flex-1 justify-center items-center'>
+        <img src={shoe8} alt="" width={540} height={522} className='object-contain'/>
+      </div>
+    </section>
   )
 }
 
